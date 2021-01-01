4651a5b412d7087cbc9f5a2f682df46f6c8f0df5
Xx-KaixX <ssdear@gmail.com>
2021-01-01 11:34:09 +0400

    Added horizontal movement and rotation function for player, also added borders to game screen

---
+++ Bounce/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{   
+    public float speed = 10.0f;
+    public Rigidbody2D rb;
+    public Vector2 movement;
+    public float RevSpeed = 100.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")
+        movement = new Vector2(Input.GetAxis("Horizontal"), 0);
+
+    }
+    void FixedUpdate()
+    {
+        MoveCharacter(movement);
+    }
+    void MoveCharacter(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * speed * Time.deltaTime));
+        
+    }
+    void RotateCharacter()
+    {
+        rb.MoveRotation(rb.rotation + RevSpeed * Time.fixedDeltaTime);
+    }
+}
