c17ea6228d11e6fb8aff0f2f5722c873d14fa007
NinjaWarrior <romenon2007@gmail.com>
2021-01-01 22:33:25 +0400

    Update PlayerMovement.cs

--- Bounce/Assets/Scripts/PlayerMovement.cs
+++ Bounce/Assets/Scripts/PlayerMovement.cs
@@ -8,29 +8,17 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     public Vector2 movement;
     public float RevSpeed = 100.0f;
-    float xMin;
-    float xMax;
-    float yMin;
-    float yMax;
+   
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
-        SetUpBoundaries();
-    }
-    private void SetUpBoundaries()
-    {
-        Camera camera = Camera.main;
-        xMin = camera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x +5f;
-        xMax = camera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - 5f;
-        yMin = camera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + 5f;
-        yMax = camera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - 5f;
     }
     // Update is called once per frame
     void Update()
     {
         //movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")
-        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movement = new Vector2(Input.GetAxis("Horizontal"), 0);
 
     }
     void FixedUpdate()
