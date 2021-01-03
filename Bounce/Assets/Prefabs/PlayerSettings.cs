1df4fe3dac7ca9db0077adafc5ea2b2412dd4074
NinjaWarrior <romenon2007@gmail.com>
2021-01-03 20:52:18 +0400

    everything is fived :)

---
+++ Bounce/Assets/Prefabs/PlayerSettings.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSettings : MonoBehaviour
+{
+    [SerializeField] string[] hexCodes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Color c = GetComponent<SpriteRenderer>().color;
+        string a = hexCodes[Random.Range(0, hexCodes.Length)];
+        if (ColorUtility.TryParseHtmlString(a, out c))
+        {
+            GetComponent<SpriteRenderer>().color = c;
+
+        }
+
+    }
+}
--- Bounce/Assets/Scripts/Player.cs
+++ Bounce/Assets/Scripts/Player.cs
@@ -5,17 +5,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] GameObject[] sprites;
-    [SerializeField] string[] hexCodes;
     [SerializeField] GameObject[] placesForSpawn;
     private void Start()
     {
         GameObject sprite = sprites[Random.Range(0, sprites.Length)];
-        Color color;
         //transform.position = placesForSpawn[Random.Range(0, placesForSpawn.Length)].transform.position;
         GameObject player = Instantiate(sprite, sprite.transform.position, Quaternion.identity) as GameObject;
-        ColorUtility.TryParseHtmlString(hexCodes[Random.Range(0, hexCodes.Length)],
-           out color);
-        sprite.GetComponent<SpriteRenderer>().color = color;
         //instantiate the prefab
 
     }
