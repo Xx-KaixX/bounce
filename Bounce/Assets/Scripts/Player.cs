92f4be69070dec83f76d7496cdf75c87982dd5bc
NinjaWarrior <romenon2007@gmail.com>
2021-01-03 20:17:10 +0400

    I have completed everything that u asked for but Im not able to get the colour to change

--- Bounce/Assets/Scripts/Player.cs
+++ Bounce/Assets/Scripts/Player.cs
@@ -7,15 +7,15 @@ public class Player : MonoBehaviour
     [SerializeField] GameObject[] sprites;
     [SerializeField] string[] hexCodes;
     [SerializeField] GameObject[] placesForSpawn;
-    [SerializeField] Color color;
     private void Start()
     {
         GameObject sprite = sprites[Random.Range(0, sprites.Length)];
-        color = sprite.GetComponent<SpriteRenderer>().color;
-        transform.position = placesForSpawn[Random.Range(0, placesForSpawn.Length)].transform.position;
-        GameObject player = Instantiate(sprite, transform.position, Quaternion.identity) as GameObject;
+        Color color;
+        //transform.position = placesForSpawn[Random.Range(0, placesForSpawn.Length)].transform.position;
+        GameObject player = Instantiate(sprite, sprite.transform.position, Quaternion.identity) as GameObject;
         ColorUtility.TryParseHtmlString(hexCodes[Random.Range(0, hexCodes.Length)],
            out color);
+        sprite.GetComponent<SpriteRenderer>().color = color;
         //instantiate the prefab
 
     }
